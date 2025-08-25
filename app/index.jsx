ee2092e0681c05d2efb3a6a0422dee2d6f1f7dc3
keetsm1 <145614621+keetsm1@users.noreply.github.com>
2025-08-25 15:36:11 -0400

    Firebase Login/Sign Up Implemented Firebase login+signup implemented only signup tested so far. Planning to add Accoutns tab to logout screen to manage user account like change password, delete account etc.

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,55 @@
-import { useRouter } from 'expo-router'
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from '@react-native-firebase/auth'
+import { Redirect, useRouter } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { Image, ImageBackground, Pressable, SafeAreaView, Text, TextInput, View } from 'react-native'
 import logo from '../assets/images/logo.png'
 import wave from '../assets/images/wave.png'
 import { authStyles } from '../styles/authStyles'
+ 
 const index = () => {
+
   const router = useRouter();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error,setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function handleAuthStateChanged(user){
+    setUser(user);
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const auth = getAuth();
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (user) return <Redirect href="/home" />
+
+  const handleLogin = async()=>{
+    try{
+      setError(null);
+      setLoading(true);
+      await signInWithEmailAndPassword(getAuth(), email.trim(), password);
+
+    }catch(e){
+      if (e?.code === 'auth/invalid-email') setError('Invalid email.');                                     
+      else if (e?.code === 'auth/user-not-found') setError('No account for this email.');               
+      else if (e?.code === 'auth/wrong-password') setError('Incorrect password.');                          
+      else setError('Login failed. Please try again.');                                                      
+      console.error(e);
+    }finally{
+      setLoading(false);
+    }
+  };
   return (
     <ImageBackground source = {wave} style = {authStyles.imageContainer}>
       <SafeAreaView style={authStyles.safeArea}>
@@ -16,17 +60,28 @@ const index = () => {
           </View>
 
           <View style= {authStyles.textInputEmail}>
-               <TextInput placeholder= "Enter your email"/>
+               <TextInput 
+               placeholder= "Enter your email"
+               autoCapitalize='none'
+               keyboardType='email-address'
+               value = {email}
+               onChangeText= {setEmail}
+               />
           </View>
           
           <View style = {authStyles.textInputPassword}>
-                <TextInput placeholder='Enter your password' secureTextEntry= {true}/>  
+                <TextInput 
+                placeholder='Enter your password' 
+                secureTextEntry= {true}
+                value = {password}
+                onChangeText= {setPassword}
+                />  
           </View>
 
 
           <View style= {authStyles.loginBtnContainer}>
-            <Pressable>
-              <Text style= {authStyles.loginText}>Login</Text>
+            <Pressable onPress= {loading ? undefined : handleLogin} disabled={loading}>
+              <Text style= {authStyles.loginText}>{loading ? 'Logging in…' : 'Login'}</Text>
             </Pressable>
           </View>
 
--- app/signup.jsx
+++ app/signup.jsx
@@ -1,8 +1,57 @@
+import { createUserWithEmailAndPassword, getAuth, signOut } from '@react-native-firebase/auth'
 import { useRouter } from 'expo-router'
+import { useState } from 'react'
 import { ImageBackground, Pressable, SafeAreaView, Text, TextInput, View } from 'react-native'
 import wave from '../assets/images/wave.png'
 import { signupStyles } from '../styles/signupStyles'
 const signup = () => {
+
+    const [email, setEmail] = useState('');
+    const [email2, setEmail2] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSignUp= async()=>{
+        if (!email || !email2 || !password || !password2){
+            setError('Please fill in all fields');
+            return;
+        }
+        if (email.trim() != email2.trim()){
+            setError('Emails do not match');
+            return;
+        }
+        if (password != password2){
+            setError('Passwords do not match');
+            return;
+        }
+
+        try{
+            setLoading(true);
+            await createUserWithEmailAndPassword(getAuth(), email.trim(), password);
+            await signOut(getAuth());
+            router.replace('/');
+        } catch (error){
+            if (error.code === 'auth/email-already-in-use'){
+                setError('That email address is already in use!');
+            }
+            else if( error.code  === 'auth/invalid-email'){
+                setError('That email address is invalid!');
+            }
+            else if(error.code === 'auth/weak-password'){
+                setError('Password should be at least 6 characters!');  
+            }
+            else{
+                setError('Something went wrong. Please try again later.');
+                console.error(error);
+            }
+        }
+        finally{
+                setLoading(false);
+            };
+    }
+
     const router = useRouter();
   return (
     <ImageBackground source = {wave} style = {signupStyles.imageContainer}>
@@ -14,26 +63,49 @@ const signup = () => {
             <View>
                 <Text style= {signupStyles.title}>Create an Account:</Text>
             </View>
+
             <View style = {signupStyles.inputContainer}>
-                <TextInput placeholder= "Enter your email"/>
+                <TextInput 
+                placeholder= "Enter your email"
+                autoCapitalize='none'
+                keyboardType='email-address'
+                value = {email}
+                onChangeText = {setEmail}
+                />
             </View>
 
             <View style = {signupStyles.inputContainer}>
-                <TextInput placeholder= "Re-enter your email"/>
+                <TextInput 
+                placeholder= "Re-enter your email"
+                autoCapitalize='none'
+                keyboardType='email-address'
+                value = {email2}
+                onChangeText = {setEmail2}
+                />
             </View>
 
             <View style = {signupStyles.inputContainer}>
-                <TextInput placeholder= "Choose a password" secureTextEntry= {true}/>
+                <TextInput 
+                placeholder= "Choose a password" 
+                secureTextEntry= {true}
+                value={password}
+                onChangeText={setPassword}
+                />
             </View>
 
 
             <View style = {signupStyles.inputContainer}>
-                <TextInput placeholder= "Re-enter the password" secureTextEntry= {true}/>
+                <TextInput 
+                placeholder= "Re-enter the password" 
+                secureTextEntry= {true}
+                value={password2}
+                onChangeText={setPassword2}
+                />
             </View>
 
             <View style= {signupStyles.loginBtnWrapper}>
-                <Pressable onPress ={null}>
-                    <Text style = {signupStyles.loginText}>LOGIN</Text>
+                <Pressable onPress ={loading ? null : handleSignUp} disabled={loading}>
+                    <Text style = {signupStyles.loginText}>{loading ? 'Creating....' : 'Create Account'}</Text>
                 </Pressable>
 
             </View>
@@ -42,9 +114,6 @@ const signup = () => {
                 <Text style={signupStyles.backToLogin}>Back to login page!</Text>
             </Pressable>
         </View>
-
-
-
      </SafeAreaView>
     </ImageBackground>
   )
